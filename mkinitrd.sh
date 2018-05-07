341e39985d28a416deed9ad90926d8215273d84d
q66 <daniel@octaforge.org>
2018-05-08 00:03:52 +0200

    support input options

--- mkinitrd.sh
+++ mkinitrd.sh
@@ -1,26 +1,49 @@
 #!/bin/sh
 
-while getopts d:s:o: opts; do
-  case ${opts} in
-    d) ROOTDEV=${OPTARG} ;;
-    s) ROOTDIR=${OPTARG} ;;
-    o) OUTPUT=${OPTARG} ;;
-  esac
+# root device
+ROOTDEV="mmcblk0p7"
+# directory on root device with the actual rootfs
+ROOTDIR="/"
+# the init program
+INIT="/sbin/init"
+
+# output file name
+OUTFILE="initrd.img"
+
+help() {
+    echo "Usage: " $0 " [arguments]"
+    echo "Available options:"
+    echo "  -h           print this message"
+    echo "  -o FILENAME  write into FILENAME (default: $OUTFILE)"
+    echo "  -d DEVICE    the partition containing rootfs (default: $ROOTDEV)"
+    echo "  -s PATH      the directory containing rootfs (default: $ROOTDIR)"
+    echo "  -i INIT      the init program to launch (default: $INIT)"
+    echo ""
+    echo "By default, rootfs is on /data (mmcblk0p7), not in any subdir."
+    echo "The -s option is intended for dual boot with Android if you want to"
+    echo "preserve your user data, while the -d option is typically for when"
+    echo "you want to install Linux onto the /system partition (mmcblk0p4)."
+    echo "Overriding -i is only for if your rootfs doesn't use /sbin/init."
+}
+
+while getopts o:d:s:i:h OPT; do
+    case $OPT in
+        o) OUTFILE=$OPTARG ;;
+        d) ROOTDEV=$OPTARG ;;
+        s) ROOTDIR=$OPTARG ;;
+        i) INIT=$OPTARG ;;
+        h) help; exit 0 ;;
+        \?)
+            echo "Unrecognized option: $OPTARG"
+            help
+            exit 1
+        ;;
+    esac
 done
 
-if [ -z "$ROOTDEV" ]; then
-  ROOTDEV=mmcblk0p7
-fi
-
-if [ -z "$ROOTDIR" ]; then
-  ROOTDIR=
-fi
-
-if [ -z "$OUTPUT" ]; then
-  OUTPUT=initrd.img
-fi
-
-export INIT=/sbin/init
+echo "Generating initrd for rootfs in $ROOTDIR on partition" \
+     "$ROOTDEV with init executable $INIT..."
+echo ""
 
 echo "Root Device: $ROOTDEV"
 echo "Root Dir: $ROOTDIR"
