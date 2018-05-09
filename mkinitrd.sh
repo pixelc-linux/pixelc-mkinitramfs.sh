8f4698567a60c8f6b21b23ee5efcec656bb4e6f6
q66 <daniel@octaforge.org>
2018-05-09 09:25:04 +0200

    make subdir root experimental and allow providing mountopts

--- mkinitrd.sh
+++ mkinitrd.sh
@@ -4,6 +4,9 @@
 ROOTDEV="mmcblk0p7"
 # directory on root device with the actual rootfs
 ROOTDIR="/"
+# mount options
+MOUNTOPTS_RW="rw,noatime,nodirtime,errors=panic"
+MOUNTOPTS="ro"
 # the init program
 INIT="/sbin/init"
 
@@ -17,20 +20,34 @@ help() {
     echo "  -o FILENAME  write into FILENAME (default: $OUTFILE)"
     echo "  -d DEVICE    the partition containing rootfs (default: $ROOTDEV)"
     echo "  -s PATH      the directory containing rootfs (default: $ROOTDIR)"
+    echo "  -m PARAMS    the options passed to mount (default: $MOUNTOPTS)"
     echo "  -i INIT      the init program to launch (default: $INIT)"
     echo ""
     echo "By default, rootfs is on /data (mmcblk0p7), not in any subdir."
-    echo "The -s option is intended for dual boot with Android if you want to"
-    echo "preserve your user data, while the -d option is typically for when"
-    echo "you want to install Linux onto the /system partition (mmcblk0p4)."
+    echo "Keep in mind that using a subdirectory might not always work right"
+    echo "and is considered EXPERIMENTAL, so use at your own risk. It exists"
+    echo "only to cover the case when you want to dual boot your system with"
+    echo "Android."
+    echo "The -m option lets you alter the options with which the filesystem"
+    echo "is mounted. By default it's 'ro' but for subdir rootfs it has to be"
+    echo "read-write, so $MOUNTOPTS_RW is used."
+    echo "The -d option allows you to choose the partition on which your root"
+    echo "filesystem is present. You might want to use this if you repartition"
+    echo "your device or if you installed the OS into /system (mmdcblk0p4)."
     echo "Overriding -i is only for if your rootfs doesn't use /sbin/init."
 }
 
-while getopts o:d:s:i:h OPT; do
+while getopts o:d:s:i:m:h OPT; do
     case $OPT in
         o) OUTFILE=$OPTARG ;;
         d) ROOTDEV=$OPTARG ;;
-        s) ROOTDIR=$OPTARG ;;
+        s)
+            ROOTDIR=$OPTARG
+            if [ -n "$ROOTDIR" ] && [ "$ROOTDIR" != "/" ]; then
+                MOUNTOPTS="$MOUNTOPTS_RW"
+            fi
+        ;;
+        m) MOUNTOPTS=$OPTARG ;;
         i) INIT=$OPTARG ;;
         h) help; exit 0 ;;
         \?)
@@ -42,7 +59,7 @@ while getopts o:d:s:i:h OPT; do
 done
 
 echo "Generating initrd for rootfs in $ROOTDIR on partition" \
-     "$ROOTDEV with init executable $INIT..."
+     "$ROOTDEV (mounted with '$MOUNTOPTS') with init executable $INIT..."
 echo ""
 
 # fetch busybox
@@ -71,12 +88,14 @@ mkdir output/dev
 mkdir output/lib
 mkdir output/sys
 mkdir -p output/mnt/root
+mkdir -p outout/mnt/data
 
 # config file
 echo "Generating config file..."
 cat << EOF >> output/conf
 export ROOTDEV="${ROOTDEV}"
 export ROOTDIR="${ROOTDIR}"
+export MOUNTOPTS="${MOUNTOPTS}"
 export INIT="${INIT}"
 EOF
 
